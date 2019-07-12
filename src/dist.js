edf3fbbc621673d5550448131a77b5dc89cf3567
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-07-12 11:56:49 -0700

    make distributions easy

---
+++ src/dist.js
@@ -0,0 +1,35 @@
+const tar = require ( 'tar' ),
+	  fs  = require( 'fs' ),
+	  pkg = require( process.cwd() + '/package.json' ),
+	  filename = 'LifterLMS.tgz',
+	  dest = 'Dash-User-Contributions/docsets/LifterLMS/',
+	  docsetfile = dest + 'docset.json',
+	  json = require( process.cwd() + '/' + dest + 'docset.json' );
+
+tar.c( {
+		gzip: true,
+		file: filename,
+	},
+	[ 'LifterLMS.docset' ]
+).then( () => {
+
+	// copy main file.
+	fs.copyFileSync( filename, dest + filename );
+
+	// copy version file.
+	fs.mkdirSync( dest + 'versions/' + pkg.version, { recursive: true } );
+	fs.copyFileSync( filename, dest + 'versions/' + pkg.version + '/' + filename );
+
+	// update the docset file to include new version info.
+	json.specific_versions.unshift( {
+		version: pkg.version,
+		archive: 'versions/' + pkg.version + '/' + filename,
+	} );
+
+	// update the docset version.
+	json.version = pkg.version;
+
+	// save the docset file changes.
+	fs.writeFileSync( docsetfile, JSON.stringify( json, null, 2 ) );
+
+} );
